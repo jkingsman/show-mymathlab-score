c818dafc298f20b181af920ee9eb070035d6e8fe
Jack Kingsman <jack.kingsman@gmail.com>
2015-01-26 13:12:47 -0800

    Remove stale icon change code

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,3 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	chrome.pageAction.setIcon({"tabId":tabId, "path":"img/question19.png"});
-	chrome.pageAction.show(tabId);
-});
-
 var links = document.getElementsByTagName("a");
 
 for(var i=0; i<links.length; i++) {
